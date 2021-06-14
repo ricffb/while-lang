8b08c712523f8f8433189f9f86257e855ff20b3d
Henrik Wachowitz <henrik.wachowitz@campus.lmu.de>
2021-06-14 12:24:58 +0200

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='while-lang',  # How you named your package folder (MyLib)
-    packages=['while'],  # Chose the same as "name"
+    packages=['whilelang'],  # Chose the same as "name"
     version=
     '0.1',  # Start with a small number and increase it with every change you make
     license=
@@ -14,15 +14,16 @@ setup(
     url=
     'https://github.com/ricffb/while-lang',  # Provide either the link to your github or to your website
     download_url=
-    'https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
-    keywords=['SOME', 'MEANINGFULL',
-              'KEYWORDS'],  # Keywords that define your package best
+    'https://github.com/ricffb/while-lang/archive/refs/tags/v0.1.tar.gz',  # I explain this later on
+    keywords=['WHILE', 'EDUCATIONAL', 'LMU', 'FSK',
+              'TIMI'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'parsy',
     ],
+    entry_points={'console_scripts': ['whilelang = whilelang.__main__:main']},
     classifiers=[
         'Development Status :: 3 - Alpha',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Students',  # Define that your audience are developers
+        'Intended Audience :: Education',  # Define that your audience are developers
         'License :: OSI Approved :: MIT License',  # Again, pick a license
         'Programming Language :: Python :: 3',  #Specify which pyhton versions that you want to support
         'Programming Language :: Python :: 3.6',
