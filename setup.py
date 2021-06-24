bf4247705a1e4f28ca6de460e76ab02bd049ac4f
Henrik Wachowitz <henrik.wachowitz@campus.lmu.de>
2021-06-24 11:33:57 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ setup(
     name='while-lang',  # How you named your package folder (MyLib)
     packages=['whilelang'],  # Chose the same as "name"
     version=
-    '0.5',  # Start with a small number and increase it with every change you make
+    '0.6',  # Start with a small number and increase it with every change you make
     license=
     'MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description=
-    'A small Parser and Interpreter for the Educational WHILE - Programming Language by Uwe Schoening.',  # Give a short description about your library
+    'A small Parser, Interpreter, and JIT Compiler for the Educational WHILE - Programming Language by Uwe Schoening.',  # Give a short description about your library
     author='Henrik Wachowitz',  # Type in your name
     author_email='henrik.wachowitz@campus.lmu.de',  # Type in your E-Mail
     url=
     'https://github.com/ricffb/while-lang',  # Provide either the link to your github or to your website
     download_url=
-    'https://github.com/ricffb/while-lang/archive/refs/tags/v0.5.tar.gz',  # I explain this later on
+    'https://github.com/ricffb/while-lang/archive/refs/tags/v0.6.tar.gz',  # I explain this later on
     keywords=['WHILE', 'EDUCATIONAL', 'LMU', 'FSK',
               'TIMI'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
