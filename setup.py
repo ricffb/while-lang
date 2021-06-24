54adc8af1313999a998d7d0ecefa8bb310d579cf
Henrik Wachowitz <henrik.wachowitz@campus.lmu.de>
2021-06-24 15:39:31 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='while-lang',  # How you named your package folder (MyLib)
     packages=['whilelang'],  # Chose the same as "name"
     version=
-    '0.7',  # Start with a small number and increase it with every change you make
+    '0.7.1',  # Start with a small number and increase it with every change you make
     license=
     'MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description=
@@ -14,7 +14,7 @@ setup(
     url=
     'https://github.com/ricffb/while-lang',  # Provide either the link to your github or to your website
     download_url=
-    'https://github.com/ricffb/while-lang/archive/refs/tags/v0.7.tar.gz',  # I explain this later on
+    'https://github.com/ricffb/while-lang/archive/refs/tags/v0.7.1.tar.gz',  # I explain this later on
     keywords=['WHILE', 'EDUCATIONAL', 'LMU', 'FSK',
               'TIMI'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
